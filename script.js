058cdef84716f388471f242cf4424a1c895a0f15
mahimakathpal <indukathpal@gmail.com>
2025-04-17 09:53:10 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+    var tl = gsap.timeline();
 tl.from(".line h1", {
     y: 150,
     stagger : 0.25,
@@ -26,7 +27,7 @@ tl.to('.line h2',{
 tl.to("#loader",{
     opacity: 0 ,
     duration: 0.2,
-    delay: 4,
+    delay: 0,
 });
 tl.from("#page1",{
     delay: 0.2,
@@ -35,7 +36,30 @@ tl.from("#page1",{
     duration: 0.5,
     ease: Power4
 
-})
+});
 tl.to("#loader" , {
     display: "none",
+});
+tl.from("#nav",{
+   opacity:0
 })
+tl.from("#hero1 h1, #hero2 h1 , #hero3 h2 ,  #hero4 h1",{
+    y: 120,
+    stagger:0.2
+})
+}
+
+function cursorAnimation() {
+
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+Shery.makeMagnet("#nav-part2 h4" );
+}
+loadingAnimation();
+cursorAnimation();
